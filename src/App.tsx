8899933cb5c1ed94e8e9f91bb177d3198327262f
gsmoff <maslovsg@ukr.net>
2022-10-13 20:48:23 +0300

    add React components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//with jsx
+const title = <h1>Hello React.js</h1>;
+
+//without jsx
+// const title = React.createElement("h1", null, "Hello React.js");
+
+const list = (
+  <ul>
+    <li>list item 1</li>
+    <li>list item 2</li>
+    <li>list item 3</li>
+  </ul>
+);
+// const List = () => (
+//   <ul>
+//     <li>list item 1</li>
+//     <li>list item 2</li>
+//     <li>list item 3</li>
+//   </ul>
+// );
+const List = () => {
+  let a = 10;
+
+  return (
+    // <React.Fragment>
+    <>
+      <ul>
+        <li>list item {a + 1}</li>
+        <li>list item 2</li>
+        <li>list item 3</li>
+      </ul>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit.</p>
+    </>
+    // </React.Fragment>
+  );
+};
+//react component
+// const Title = () => <h1>Hello React component</h1>;
+
+//react props
+type TitleProps = {
+  title: string;
+  // text?: string;
+  text: string;
+  num: number;
+};
+
+const Title = (props: TitleProps) => (
+  <>
+    <h1>
+      Hello {props.text} {props.title}
+    </h1>
+    <div>{props.num}</div>
+  </>
+);
+
+// const Title = (props: TitleProps) => {
+//   console.log(props);
+//   return (
+//     <h1>
+//       Hello {props.text} {props.title}
+//     </h1>
+//   );
+// };
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Title /> */}
+      <List />
+      <Title title="App.js" text="Hi!" num={15} />
+      <Title title="Test.js" text="Hello!" num={25} />
+      {title} {list} {10 + 5}
     </div>
   );
 }
