a38ca5fe23eb22594d2e9c58cae53a1d0b58f385
gsmoff <maslovsg@ukr.net>
2022-10-14 19:19:12 +0300

    add lesson 3

---
+++ src/conteiner/App/App.tsx
@@ -0,0 +1,35 @@
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hello App!</h1>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// import React from 'react'
+
+// type Props = {}
+
+// export const App = (props: Props) => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
