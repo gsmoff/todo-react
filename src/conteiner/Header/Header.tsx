bf63441e40ce0a1996908e194dbaab714470b680
gsmoff <maslovsg@ukr.net>
2022-10-14 20:55:02 +0300

    add menu components

--- src/conteiner/Header/Header.tsx
+++ src/conteiner/Header/Header.tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Menu from "../../components/Menu";
+
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Container maxWidth="lg">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Fake Shop
+            </Typography>
+            <Menu/>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
