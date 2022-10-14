006f5137bc2bf5a2c6306f134e9eca8dc33525a6
gsmoff <maslovsg@ukr.net>
2022-10-14 19:51:41 +0300

    add cra

--- src/conteiner/App/App.tsx
+++ src/conteiner/App/App.tsx
@@ -1,35 +1,21 @@
-// import React from "react";
-
-// const App = () => {
-//   return (
-//     <>
-//       <h1>Hello App!</h1>
-//     </>
-//   );
-// };
-
-// export default App;
-
-// import React from 'react'
-
-// type Props = {}
-
-// export const App = (props: Props) => {
-//   return (
-//     <div>App</div>
-//   )
-// }
-
-// export default App
-
 import React from 'react'
+import CssBaseline from "@mui/material/CssBaseline";
+
+import Header from "../../Footer/footer";
+import Main from "../../Main/Main";
+import Footer from "../../Header/Header";
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
-    <div>App</div>
-  )
+    <>
+      <CssBaseline />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App
