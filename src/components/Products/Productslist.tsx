2955e728d3335e95eee8e6a1160dbfa454c2d5d4
gsmoff <maslovsg@ukr.net>
2022-10-20 19:56:15 +0300

    add map props

--- src/components/Products/Productslist.tsx
+++ src/components/Products/Productslist.tsx
@@ -2,6 +2,14 @@ import { Grid, Typography } from "@mui/material";
 import React from "react";
 import ProductsListItem from "components/Products/ProductsListItem";
 import productsArray from "components/Products/productsArray";
+
+type ProductProps = {
+  name: string;
+  description: string;
+  type: string;
+  capacity: number;
+  price: number;
+};
 type Props = {};
 
 const Productslist = (props: Props) => {
@@ -28,17 +36,19 @@ const Productslist = (props: Props) => {
         alignItems="center"
         spacing={4}
       >
-        {productsArray.map((product) => (
-          <Grid item xs={12} sm={6} md={4}>
-            <ProductsListItem
-              name={product.name}
-              description={product.description}
-              type={product.type}
-              capacity={product.capacity}
-              price={product.price}
-            />
-          </Grid>
-        ))}
+        {productsArray.map(
+          ({ name, description, type, capacity, price, }: ProductProps) => (
+            <Grid item xs={12} sm={6} md={4}>
+              <ProductsListItem
+                name={name}
+                description={description}
+                type={type}
+                capacity={capacity}
+                price={price}
+              />
+            </Grid>
+          )
+        )}
       </Grid>
     </>
   );
