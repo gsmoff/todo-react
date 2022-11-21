64c42981577b8ac5d613fe1b47f416729000ffe1
gsmoff <maslovsg@ukr.net>
2022-11-22 01:48:12 +0200

    fix

--- src/conteiner/App/App.tsx
+++ src/conteiner/App/App.tsx
@@ -1,36 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import CssBaseline from '@mui/material/CssBaseline'
-import Header from '../Header/Header'
-import Main from '../Main/Main'
-import Footer from '../Footer/Footer'
-import { StyledEngineProvider } from '@mui/material/styles'
-import { count } from 'console'
-import { omit } from 'lodash'
-import { useAppDispatch } from 'redux/hooks'
-import { fetchProducts } from 'redux/productsReducer'
-import { Routes, Route } from 'react-router-dom'
-import Home from '../../pages/Home/Home'
+import React, { useEffect, useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { count } from "console";
+import { omit } from "lodash";
+import { useAppDispatch } from "redux/hooks";
+import { fetchProducts } from "redux/productsReducer";
 
 // @ts-ignore
 
 const App = (props: Props) => {
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        dispatch(fetchProducts())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
-    return (
-        <StyledEngineProvider injectFirst>
-            <CssBaseline />
-            <Header />
-            <Main />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-            <Footer />
-        </StyledEngineProvider>
-    )
-}
+  return (
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <Header />
+      <Main />
+      {/* <Footer /> */}
+    </StyledEngineProvider>
+  );
+};
 
-export default App
+export default App;
--- src/conteiner/Main/Main.tsx
+++ src/conteiner/Main/Main.tsx
@@ -10,19 +10,19 @@ import CheckoutPage from "pages/Checkout/CheckoutPage";
 
 const Main = () => {
   return (
-    <main>
-      <Container maxWidth="lg">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="shipping" element={<ShippingPage />} />
-          <Route path="payment" element={<PaymentPage />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-        </Routes>
-      </Container>
-    </main>
-  );
+      <main>
+          <Container maxWidth="lg">
+              <Routes>
+                  <Route path="*" element={<Home />} />
+                  <Route path="cart" element={<CartPage />} />
+                  <Route path="about" element={<AboutPage />} />
+                  <Route path="shipping" element={<ShippingPage />} />
+                  <Route path="payment" element={<PaymentPage />} />
+                  <Route path="checkout" element={<CheckoutPage />} />
+              </Routes>
+          </Container>
+      </main>
+  )
 };
 
 export default Main;
